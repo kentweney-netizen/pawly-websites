62b55136de1c65f78ad3ad7d2b33387389a17c0d
kentweney-netizen <kentweney@gmail.com>
2026-06-16 23:24:59 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,13 @@
-const CACHE_NAME = 'pawly-pwa-v1';
+const CACHE_NAME = 'pawly-pets-v1';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
-      ]);
-    })
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
