82f2481702e7eef0d64f11bb232f520b4b449952
kentweney-netizen <kentweney@gmail.com>
2026-07-03 19:31:06 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,40 @@
-const CACHE_NAME = 'pawly-pwa-v9';
+// service-worker.js
+const CACHE_NAME = 'pawly-pets-v3';
+const urlsToCache = [
+  '/',
+  '/index.html'
+  // 如果以后有其他静态资源（如图片、CSS），可以在这里添加
+];
 
-self.addEventListener('install', event => {
-  self.skipWaiting();
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting(); // 强制立即激活新版本
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // 删除旧缓存
           }
         })
-      )
-    )
+      );
+    })
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  const dest = event.request.destination;
-
-  if (dest === 'image' || dest === 'style' || dest === 'script' || dest === 'font') {
-    event.respondWith(
-      caches.match(event.request).then(cached => {
-        if (cached) return cached;
-
-        return fetch(event.request).then(response => {
-          if (response && response.status === 200) {
-            const clone = response.clone();
-            caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-          }
-          return response;
-        });
-      })
-    );
-  } else {
-    event.respondWith(fetch(event.request));
-  }
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // 有缓存就用缓存，没有就去网络请求
+      return response || fetch(event.request);
+    })
+  );
 });
