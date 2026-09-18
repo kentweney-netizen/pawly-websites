a10fa84752d4d56f007bac8a2a01b234ef7bdc67
kentweney-netizen <kentweney@gmail.com>
2026-09-18 13:42:51 +0800

    Stop compiling unused myth image barrel

--- dapp/src/petHubMythImg.ts
+++ dapp/src/petHubMythImg.ts
@@ -1,10 +1,2 @@
-import fox from "./petHubMythFox";
-import moth from "./petHubMythMoth";
-import wyrm from "./petHubMythWyrm";
-import boar from "./petHubMythBoar";
-import cat from "./petHubMythCat";
-import toad from "./petHubMythToad";
-import lynx from "./petHubMythLynx";
-import rose from "./petHubMythRose";
-
-export const MYTH_IMG = { fox, moth, wyrm, boar, cat, toad, lynx, rose } as const;
+/** Unused barrel. Portraits load from /myth/*.jpg */
+export {};
