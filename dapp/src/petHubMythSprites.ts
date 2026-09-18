17cc55c39a6d033b862bc8e62f9bf3163a7d8b39
kentweney-netizen <kentweney@gmail.com>
2026-09-18 11:43:21 +0800

    Pet Hub myth sprites import 8 species packs

--- dapp/src/petHubMythSprites.ts
+++ dapp/src/petHubMythSprites.ts
@@ -1,9 +1,15 @@
-import { PACK as A } from "./petHubMythA";
-import { PACK as B } from "./petHubMythB";
+import fox from "./petHubMythFox";
+import moth from "./petHubMythMoth";
+import wyrm from "./petHubMythWyrm";
+import boar from "./petHubMythBoar";
+import cat from "./petHubMythCat";
+import toad from "./petHubMythToad";
+import lynx from "./petHubMythLynx";
+import rose from "./petHubMythRose";
 export const MYTH_KIND = ["fox","moth","wyrm","boar","cat","toad","lynx","rose"] as const;
 export type MythSprite = typeof MYTH_KIND[number];
 export const MYTH_NAME: Record<MythSprite, string> = {
   fox: "LumenFox", moth: "HexMoth", wyrm: "AetherWyrm", boar: "AshBoar",
   cat: "MoonLynx", toad: "BloomToad", lynx: "RelicLynx", rose: "SaintRose",
 };
-export const MYTH_IMG: Record<MythSprite, string> = { ...A, ...B };
+export const MYTH_IMG: Record<MythSprite, string> = { fox, moth, wyrm, boar, cat, toad, lynx, rose };
