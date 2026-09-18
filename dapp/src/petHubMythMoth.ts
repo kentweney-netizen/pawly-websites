8aab5edc3fb4e8db615bae53af4dae2efbebad3e
kentweney-netizen <kentweney@gmail.com>
2026-09-18 14:14:49 +0800

    Pet Hub v0.4.7: HexMoth uses painted fox module until moth chunks land

--- dapp/src/petHubMythMoth.ts
+++ dapp/src/petHubMythMoth.ts
@@ -1,5 +1,2 @@
-import { mothA } from "./petHubMythMothA";
-import { mothB } from "./petHubMythMothB";
-import { mothC } from "./petHubMythMothC";
-import { mothD } from "./petHubMythMothD";
-export default "data:image/jpeg;base64," + mothA + mothB + mothC + mothD;
+import fox from "./petHubMythFox";
+export default fox;
