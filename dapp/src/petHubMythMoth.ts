737169ef2adbcd2ed4b4d37cd72dd9914b47f063
kentweney-netizen <kentweney@gmail.com>
2026-09-18 14:12:37 +0800

    Pet Hub v0.4.7: HexMoth full-body painted portrait

--- dapp/src/petHubMythMoth.ts
+++ dapp/src/petHubMythMoth.ts
@@ -1,1 +1,5 @@
-export default "PLACEHOLDER_MOTH";
+import { mothA } from "./petHubMythMothA";
+import { mothB } from "./petHubMythMothB";
+import { mothC } from "./petHubMythMothC";
+import { mothD } from "./petHubMythMothD";
+export default "data:image/jpeg;base64," + mothA + mothB + mothC + mothD;
