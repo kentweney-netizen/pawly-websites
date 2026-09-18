76ecf83d6e867c76886cdb359501272fc4481dc0
kentweney-netizen <kentweney@gmail.com>
2026-09-18 14:12:35 +0800

    Pet Hub v0.4.7: BloomToad full-body painted portrait

--- dapp/src/petHubMythToad.ts
+++ dapp/src/petHubMythToad.ts
@@ -1,1 +1,4 @@
-export default "PLACEHOLDER_TOAD";
+import { toadA } from "./petHubMythToadA";
+import { toadB } from "./petHubMythToadB";
+import { toadC } from "./petHubMythToadC";
+export default "data:image/jpeg;base64," + toadA + toadB + toadC;
