ac8a61d0f0d7fc892d72da9177789588554f94ac
kentweney-netizen <kentweney@gmail.com>
2026-09-18 13:41:42 +0800

    Pet Hub v0.4.5 myth portrait barrel

---
+++ dapp/src/petHubMythImg.ts
@@ -0,0 +1,10 @@
+import fox from "./petHubMythFox";
+import moth from "./petHubMythMoth";
+import wyrm from "./petHubMythWyrm";
+import boar from "./petHubMythBoar";
+import cat from "./petHubMythCat";
+import toad from "./petHubMythToad";
+import lynx from "./petHubMythLynx";
+import rose from "./petHubMythRose";
+
+export const MYTH_IMG = { fox, moth, wyrm, boar, cat, toad, lynx, rose } as const;
