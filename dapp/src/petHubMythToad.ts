003e0e60a2aa08b59fafc9a24bbf69026dcccea5
kentweney-netizen <kentweney@gmail.com>
2026-09-18 14:17:47 +0800

    Pet Hub v0.4.7: fix BloomToad base64 missing z

--- dapp/src/petHubMythToad.ts
+++ dapp/src/petHubMythToad.ts
@@ -1,4 +1,4 @@
 import { toadA } from "./petHubMythToadA";
 import { toadB } from "./petHubMythToadB";
 import { toadC } from "./petHubMythToadC";
-export default "data:image/jpeg;base64," + toadA + toadB + toadC;
+export default "data:image/jpeg;base64," + toadA + toadB.replace("NW0Zrwk", "NW0zZrwk") + toadC;
